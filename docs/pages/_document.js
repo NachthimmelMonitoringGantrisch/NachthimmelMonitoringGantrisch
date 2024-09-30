4150b8daeca82b837e8d691c811e452b3ef15a02
naethu <nathan.matzinger@gmail.com>
2024-09-30 19:49:39 +0200

    Create _document.js

---
+++ docs/pages/_document.js
@@ -0,0 +1,16 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <link rel="icon" href="/images/gantrisch.svg" type="image/svg+xml" />
+            </Head>
+            <body>
+            <Main />
+            <NextScript />
+            </body>
+        </Html>
+    )
+}
