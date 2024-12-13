b3d5b55ef0c9a2cb39891d456c1e910502a082bc
naamp <nando.amport@gmail.com>
2024-12-13 01:50:00 +0100

    Update MinMaxMSAS_DarkTimeMoon_perMonth.R
    - Change to night_id
    - optimize loading time

--- r/Shiny_Gantrisch/MinMaxMSAS_DarkTimeMoon_perMonth.R
+++ r/Shiny_Gantrisch/MinMaxMSAS_DarkTimeMoon_perMonth.R
@@ -19,7 +19,7 @@ load_data_from_database <- function(photometer_id) {
   
   conn <- dbConnect(RSQLite::SQLite(), db_tess_data_path)
   table_name <- paste0(photometer_id, "_data")
-  query <- paste("SELECT * FROM", table_name)
+  query <- paste("SELECT time, msas, sun_alt, moon_illumination, night_id FROM", table_name)
   df <- dbGetQuery(conn, query)
   dbDisconnect(conn)
   
@@ -32,88 +32,70 @@ load_data_from_database <- function(photometer_id) {
 
 # Function to calculate total time per night based on provided photometer, date range, and msas threshold
 calculate_total_time_per_night <- function(data, xmin_date, xmax_date, msas_threshold = 21.3, time_diff_threshold = 200) {
- 
-  # Ensure the 'time' column is in the correct datetime format
   data <- data %>%
-    mutate(time = as.POSIXct(time, format = "%Y-%m-%d %H:%M:%OS", tz = "UTC")) %>%
-    arrange(time)  # Ensure data is sorted by time
+    mutate(time = as.POSIXct(time, format = "%Y-%m-%d %H:%M:%OS")) %>%  
+    arrange(time)  
   
-  # Calculate time difference in seconds between consecutive entries and remove rows where time_diff_next exceeds the threshold (e.g., 200 seconds)
   data <- data %>%
     mutate(time_diff_next = c(as.numeric(difftime(time[-1], time[-n()], units = "secs")), NA)) %>%
     filter(is.na(time_diff_next) | time_diff_next <= time_diff_threshold)
-
-  # Filter out rows where msas is less than the threshold
+  
   data_filtered <- data %>%
     filter(msas >= msas_threshold)
   
-  # Filter the data for the specified date range
   data_filtered_range <- data_filtered %>%
     filter(as.Date(time) >= xmin_date & as.Date(time) <= xmax_date)
   
-  # Create a new 'night' grouping variable by using floor_date to group by midnight (12:00 AM)
-  data_filtered_range <- data_filtered_range %>%
-    mutate(night = floor_date(time, "day"))  # Group by each night starting from 12:00 AM
-  
-  # Summarize total time per night
   data_night_summary <- data_filtered_range %>%
-    group_by(night) %>%  # Group by night
-    summarise(total_time_sec = sum(time_diff_next, na.rm = TRUE))  # Sum the time differences for each night
+    group_by(night_id) %>%  
+    summarise(total_time_sec = sum(time_diff_next, na.rm = TRUE))  
   
-  # Convert total time from seconds to hours for better readability
   data_night_summary$total_time_hours <- data_night_summary$total_time_sec / 3600
   
   return(data_night_summary)
 }
 
 process_data <- function(df, month) {
-  # Parse the input month and extract year and month
-  parsed_date <- ym(month)  # Parse "YYYY-MM" to a date
+  parsed_date <- ym(month)  
   selected_year <- year(parsed_date)
   selected_month <- month(parsed_date)
-
+  
   df <- df %>%
     mutate(
-      time = as.POSIXct(time, format = "%Y-%m-%d %H:%M:%S", tz = "UTC"),
-      night_id = as.integer(difftime(time, as.POSIXct("1970-01-01 20:00:00", tz = "UTC"), units = "days")) +
-        ifelse(hour(time) < 9, -1, 0)
+      time = as.POSIXct(time, format = "%Y-%m-%d %H:%M:%S")  
     ) %>%
     filter(
-      sun_alt <= -18,         # Filter for sun_alt based on variable
-      year(time) == selected_year,      # Filter for the selected year
-      month(time) == selected_month,    # Filter for the selected month
-      msas > 0            # Exclude values below the minimum MSAS
+      sun_alt <= -18,         
+      year(time) == selected_year,      
+      month(time) == selected_month,    
+      msas > 0            
     )
-
-  # Group by night_id and calculate min and max msas for each night
+  
   df_night_stats <- df %>%
     group_by(night_id) %>%
     summarise(
-      date = as.Date(min(time)),           # Use the minimum time for the date
-      min_msas = min(msas, na.rm = TRUE),  # Minimum MSAS for the night
-      max_msas = max(msas, na.rm = TRUE)   # Maximum MSAS for the night
+      date = as.Date(min(time)),           
+      min_msas = min(msas, na.rm = TRUE),  
+      max_msas = max(msas, na.rm = TRUE)   
     ) %>%
     ungroup()
-
+  
   return(df_night_stats)
 }
 
 plot_month <- function(df_night_stats, month) {
-  # Define Bortle scale colors and MSAS ranges
   bortle_scale <- data.frame(
     min_msas = c(21.90, 21.50, 21.30, 20.80, 20.10, 19.10, 18.00, -Inf),
     max_msas = c(Inf, 21.90, 21.50, 21.30, 20.80, 20.10, 19.10, 18.00),
     color = c("#2E2E2E", "#404040", "#0000FF", "#00FF00", "#FFFF00", "#FFA500", "#FF0000", "#FFFFFF"),
     bortle_class = c("1", "2", "3", "4", "4.5", "5", "6-7", "8-9")
   )
-
-  # Define the date range for the plot
-  xmin_date <- min(df_night_stats$date)
-  xmax_date <- max(df_night_stats$date)
-
-  # Plot min and max msas values for each night in the specified month
+  
+  parsed_date <- ym(month)
+  xmin_date <- floor_date(as.Date(parsed_date), "month")
+  xmax_date <- ceiling_date(as.Date(parsed_date), "month") - 1
+  
   p1 <- ggplot(df_night_stats, aes(x = date)) +
-    # Add Bortle scale as background colors
     geom_rect(data = bortle_scale, aes(xmin = xmin_date, xmax = xmax_date, ymin = min_msas, ymax = max_msas, fill = bortle_class), 
               alpha = 0.1, inherit.aes = FALSE) +
     scale_fill_manual(
@@ -125,13 +107,9 @@ plot_month <- function(df_night_stats, month) {
     geom_line(aes(y = min_msas, color = "Min"), size = 1.2) +
     geom_line(aes(y = max_msas, color = "Max"), size = 1.2) +
     annotate("text", x = xmax_date, y = 21.3, label = "MSAS 21.3 Grenze", color = "red", hjust = 0.8, vjust = -1.5, size = 4) +
-    labs(
-      x = NULL,
-      y = "MSAS [mag/arcsec²]",
-      color = "MSAS Werte"
-    ) +
+    labs(x = NULL, y = "MSAS [mag/arcsec²]", color = "MSAS Werte") +
     scale_y_continuous(limits = c(15, 24), breaks = seq(15, 24, by = 1)) +
-    scale_x_date(breaks = seq(xmin_date, xmax_date, by = "1 day")) +
+    scale_x_date(limits = c(xmin_date, xmax_date), breaks = seq(xmin_date, xmax_date, by = "1 day")) +
     theme_minimal() +
     theme(
       axis.text.x = element_blank(),
@@ -147,9 +125,22 @@ plot_month <- function(df_night_stats, month) {
   return(p1)
 }
 
-plot_bar <- function(data_night_summary) {
-  p2 <- ggplot(data_night_summary, aes(x = night, y = total_time_hours)) +
-    geom_bar(stat = "identity", fill = "#8a8a8a") +
+plot_bar <- function(data_night_summary, month) {
+  parsed_date <- ym(month)
+  xmin_date <- floor_date(as.Date(parsed_date), "month")
+  xmax_date <- ceiling_date(as.Date(parsed_date), "month") - 1
+  
+  # Convert night_id to start and end dates
+  data_night_summary <- data_night_summary %>%
+    mutate(
+      start_date = as.Date(substr(night_id, 2, 9), "%Y%m%d"),
+      end_date = start_date + 1
+    )
+  
+  # Create the bar plot with fixed x-axis range for the month
+  p2 <- ggplot(data_night_summary, aes(xmin = start_date, xmax = end_date, ymin = 0, ymax = total_time_hours)) +
+    geom_rect(fill = "#8a8a8a", color = "black", alpha = 0.8) +
+    scale_x_date(limits = c(xmin_date, xmax_date), breaks = seq(xmin_date, xmax_date, by = "1 day")) +
     labs(
       title = NULL,
       y = "Zeittotal MSAS > 21.3 [h]",
@@ -164,26 +155,33 @@ plot_bar <- function(data_night_summary) {
       panel.grid.minor.x = element_blank(),
       panel.grid.major.y = element_line(color = "lightgrey", size = 0.5),
       panel.grid.minor.y = element_blank()
-    ) +
-    scale_x_datetime(date_labels = "%b %d", date_breaks = "1 day")
+    )
   
   return(p2)
 }
 
-plot_line <- function(data, xmin_date, xmax_date) {
+plot_line <- function(data, month) {
+  parsed_date <- ym(month)  # Parse the input month
+  xmin_date <- floor_date(as.Date(parsed_date), "month")
+  xmax_date <- ceiling_date(as.Date(parsed_date), "month") - 1
+  
   data_filtered <- data %>%
     mutate(time = as.POSIXct(time, format = "%Y-%m-%d %H:%M:%OS", tz = "UTC")) %>%
     filter(time >= xmin_date & time <= xmax_date)
-
+  
   p3 <- ggplot(data_filtered, aes(x = time, y = moon_illumination, color = moon_illumination)) +
     geom_line(color = "#8a8a8a", size = 1.2) +
     labs(
       x = "Tag",
       y = "Mondphase"
     ) +
-    theme_minimal() +
+    scale_x_datetime(
+      limits = c(as.POSIXct(xmin_date), as.POSIXct(xmax_date)),
+      date_labels = "%b %d",
+      date_breaks = "1 day"
+    ) +
     scale_y_continuous(limits = c(0, 1), breaks = c(0, 1)) +
-    scale_x_datetime(date_labels = "%b %d", date_breaks = "1 day") +
+    theme_minimal() +
     theme(
       axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 0.5),
       panel.grid.minor.x = element_line(color = "lightgrey", size = 0.5),
@@ -191,7 +189,7 @@ plot_line <- function(data, xmin_date, xmax_date) {
       panel.grid.major.y = element_line(color = "lightgrey", size = 0.5),
       panel.grid.minor.y = element_blank()
     )
-
+  
   return(p3)
 }
 
@@ -202,14 +200,14 @@ main <- function(photometer_id, month) {
   yearly_counts <- process_data(data, month)
   
   # Calculate xmin_date and xmax_date for the selected month
-  xmin_date <- min(yearly_counts$date)
-  xmax_date <- max(yearly_counts$date)
+  xmin_date <- floor_date(as.Date(ym(month)), "month")
+  xmax_date <- ceiling_date(as.Date(ym(month)), "month") - 1
 
   data_night_summary <- calculate_total_time_per_night(data, xmin_date, xmax_date + 1)
 
   p1 <- plot_month(yearly_counts, month)
-  p2 <- plot_bar(data_night_summary)
-  p3 <- plot_line(data, xmin_date, xmax_date)
+  p2 <- plot_bar(data_night_summary, month)
+  p3 <- plot_line(data, month)
   
   # Parse the input month to get a formatted string for the title
   parsed_date <- ym(month)
